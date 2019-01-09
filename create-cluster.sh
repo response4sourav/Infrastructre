1be467f158e6949f4c4640b51bb94b6821fba04d
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-09 14:40:20 +0000

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -9,9 +9,9 @@ fi
 #export variables like project name, region and tf_state
 source ../common.sh
 
-../tfw init -backend-config="project=${PROJECT}" -backend-config="path=${TF_STATE_PATH}"
+./tfw init -backend-config="project=${PROJECT}" -backend-config="path=${TF_STATE_PATH}"
 if [[ $1 == 'plan' ]]; then
-  ../tfw plan -var="project=${PROJECT}" -var="region=${REGION}" -out=cluster.tfplan
+  ./tfw plan -var="project=${PROJECT}" -var="region=${REGION}" -out=cluster.tfplan
 elif [[ $1 == 'apply' ]]; then
-  ../tfw apply cluster.tfplan
+  ./tfw apply cluster.tfplan
 fi
