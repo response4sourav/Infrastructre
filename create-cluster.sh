c678321fc7302c1d735a557cd3ea05148c5d907e
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-09 14:39:49 +0000

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -2,27 +2,16 @@
 set -e
 
 if [[ -z "${1}" ]]; then
-  echo "Please enter the cluster name " >&2
+  echo "Please choose terraform plan or apply ?" >&2
   exit 1
 fi
 
-if [[ -z "${2}" ]]; then
-  echo "Are you planning or applying terraform ?" >&2
-  exit 1
-fi
-
-if [[ ! -d "${1}" ]]; then
-  echo "Directory not found for the cluster ${1}" >&2
-  exit 1
-fi
-
+#export variables like project name, region and tf_state
 source ../common.sh
 
-cd ${1}
-
 ../tfw init -backend-config="project=${PROJECT}" -backend-config="path=${TF_STATE_PATH}"
-if [[ $2 == 'plan' ]]; then
+if [[ $1 == 'plan' ]]; then
   ../tfw plan -var="project=${PROJECT}" -var="region=${REGION}" -out=cluster.tfplan
-elif [[ $2 == 'apply' ]]; then
+elif [[ $1 == 'apply' ]]; then
   ../tfw apply cluster.tfplan
 fi
