7525f3428509aa6b1cda23fcd0635b884cdf08c6
Sourav Saha <response4sourav@gmail.com>
2019-01-11 14:15:23 +0000

    Save TF State in GCS Bucket

---
+++ tf_files/gcs_bucket.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "gcs" {
+  # bucket  = must be provided by -backend-config option to 'terraform init'
+  # path    = must be provided by -backend-config option to 'terraform init'
+  # project = must be provided by -backend-config option to 'terraform init'
+  }
+}
