ce2f3c27c8f6ab681f537a4161c68c32a1a40dc5
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-11 14:49:18 +0000

    Update gcs_bucket.tf

--- tf_files/gcs_bucket.tf
+++ tf_files/gcs_bucket.tf
@@ -1,3 +1,8 @@
+provider "google" {
+ credentials = "${file("../../gocd-credentials.json")}"
+ project     = "${var.project}"
+}
+
 terraform {
   backend "gcs" {
   # bucket  = must be provided by -backend-config option to 'terraform init'
