1ac4b9a26de310a82a94688088e01e24697afd93
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-11 17:56:41 +0000

    Update gcs_bucket.tf

--- tf_files/gcs_bucket.tf
+++ tf_files/gcs_bucket.tf
@@ -3,9 +3,5 @@ terraform {
   # bucket  = must be provided by -backend-config option to 'terraform init'
   # path    = must be provided by -backend-config option to 'terraform init'
   # project = must be provided by -backend-config option to 'terraform init'
-    credentials = "${file("../../gocd-credentials.json")}"
-	project     = "woven-framework-218313"
-	bucket      = "woven-framework-218313-tf-state"
-	prefix      = "tf_state"
   }
 }
