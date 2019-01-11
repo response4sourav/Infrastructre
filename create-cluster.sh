3224ad7848f23515e0eac4df61fc76472d5195ef
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-11 17:53:27 +0000

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -26,7 +26,8 @@ create-tfstate-bucket || true
 
 cd $TF_DIR
 
-terraform init -backend-config='credentials=file("../../gocd-credentials.json")' -backend-config="project=${PROJECT}" -backend-config="bucket=${BUCKET_NAME}" -backend-config="prefix=${TF_STATE_PATH}"
+#terraform init -backend-config="project=${PROJECT}" -backend-config="bucket=${BUCKET_NAME}" -backend-config="prefix=${TF_STATE_PATH}"
+terraform init 
 if [[ $1 == 'plan' ]]; then
   terraform plan -var="project=${PROJECT}" -var="region=${REGION}" -var="zone=${ZONE}" -var="name=${NAME}" -out=cluster.tfplan
 elif [[ $1 == 'apply' ]]; then
