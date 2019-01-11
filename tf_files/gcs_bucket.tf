f570eb69c2cccf34deff9415bf5c75132dd200c1
response4sourav <46452600+response4sourav@users.noreply.github.com>
2019-01-11 17:27:21 +0000

    Update gcs_bucket.tf

--- tf_files/gcs_bucket.tf
+++ tf_files/gcs_bucket.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "gcs" {
-    credentials = "${file("../../gocd-credentials.json")}"
     # bucket  = must be provided by -backend-config option to 'terraform init'
     # path    = must be provided by -backend-config option to 'terraform init'
     # project = must be provided by -backend-config option to 'terraform init'    
